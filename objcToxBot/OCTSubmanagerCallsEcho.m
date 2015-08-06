56a1f0d469ab4a568513acca576bd5a8eb50b1d8
Chuong Vu <chuongv@gmail.com>
2015-08-05 18:11:56 -0700

    Some progress with echo video bot

--- objcToxBot/OCTSubmanagerCallsEcho.m
+++ objcToxBot/OCTSubmanagerCallsEcho.m
@@ -9,11 +9,20 @@
 #import "OCTSubmanagerCallsEcho.h"
 #import <objcTox/OCTToxAV.h>
 
+@interface OCTSubmanagerCallsEcho ()
+
+@property (nonatomic, assign) uint8_t *uChromaPlane;
+@property (nonatomic, assign) uint8_t *vChromaPlane;
+@property (nonatomic, assign) size_t sizeOfChromaPlanes;
+@property (nonatomic, assign) uint8_t *yLumaPlane;
+@property (nonatomic, assign) size_t sizeOfYLumaPlanes;
+
+@end
 @implementation OCTSubmanagerCallsEcho
 
 - (void)toxAV:(OCTToxAV *)toxAV receiveCallAudioEnabled:(BOOL)audio videoEnabled:(BOOL)video friendNumber:(OCTToxFriendNumber)friendNumber
 {
-    [toxAV answerIncomingCallFromFriend:friendNumber audioBitRate:48 videoBitRate:400 error:nil];
+    [toxAV answerIncomingCallFromFriend:friendNumber audioBitRate:48 videoBitRate:3000 error:nil];
 }
 
 - (void)   toxAV:(OCTToxAV *)toxAV
@@ -34,12 +43,71 @@
                        vStride:(OCTToxAVStrideData)vStride
                   friendNumber:(OCTToxFriendNumber)friendNumber
 {
+
+    size_t numberOfElementsForChroma = width * height / 2;
+    /**
+     * Recreate the buffers if the original ones are too small
+     */
+    if (numberOfElementsForChroma > self.sizeOfChromaPlanes) {
+
+        if (self.uChromaPlane) {
+            free(self.uChromaPlane);
+        }
+
+        if (self.vChromaPlane) {
+            free(self.vChromaPlane);
+        }
+
+        self.uChromaPlane = malloc(numberOfElementsForChroma * sizeof(uint8_t));
+        self.vChromaPlane = malloc(numberOfElementsForChroma * sizeof(uint8_t));
+
+        self.sizeOfChromaPlanes = numberOfElementsForChroma;
+    }
+
+    size_t sizeOfYPlane = width * height;
+
+    if (sizeOfYPlane > self.sizeOfYLumaPlanes) {
+        if (self.yLumaPlane) {
+            free(self.yLumaPlane);
+        }
+
+        self.yLumaPlane = malloc(sizeOfYPlane);
+        self.sizeOfYLumaPlanes = sizeOfYPlane;
+    }
+
+    /**
+     * Deinterleaved the UV planes and place them to in the reusable arrays
+     */
+    uint8_t *uSource = (uint8_t *)uPlane;
+    uint8_t *vSource = (uint8_t *)vPlane;
+    uint8_t *uDestination = self.uChromaPlane;
+    uint8_t *vDestination = self.vChromaPlane;
+
+    uint8_t *ySource = (uint8_t *)yPlane;
+    uint8_t *yDestination = self.yLumaPlane;
+
+    for (size_t pixelHeight = 0; pixelHeight < height; pixelHeight++) {
+        memcpy(yDestination, ySource, width);
+        ySource += yStride;
+        yDestination += width;
+
+
+        for (size_t pixelWidth = 0; pixelWidth < width; pixelWidth++) {
+            uDestination[pixelWidth] = uSource[pixelWidth];
+            vDestination[pixelWidth] = vSource[pixelWidth];
+        }
+        uDestination += width / 2;
+        vDestination += width / 2;
+        uSource += uStride;
+        vSource += vStride;
+    }
+
     [toxAV sendVideoFrametoFriend:friendNumber
                             width:width
                            height:height
-                           yPlane:yPlane
-                           uPlane:uPlane
-                           vPlane:vPlane
+                           yPlane:self.yLumaPlane
+                           uPlane:self.uChromaPlane
+                           vPlane:self.vChromaPlane
                             error:nil];
 }
 
